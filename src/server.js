b5825904e69f906d3f989d222aa6927cf7fef6a6
Arif Rahman Hakim <iceteasweeet@gmail.com>
2022-05-07 08:08:45 +0700

    server-side caching implemented

--- src/server.js
+++ src/server.js
@@ -24,10 +24,12 @@ const ProducerService = require('./services/rabbitmq/ProducerService');
 const ExportsValidator = require('./validator/exports');
 const StorageService = require('./services/S3/StorageService');
 const UploadsValidator = require('./validator/uploads');
+const CacheService = require('./services/redis/CacheService');
 
 const init = async () => {
-  const collaborationsService = new CollaborationsService();
-  const notesService = new NotesService(collaborationsService);
+  const cacheService = new CacheService();
+  const collaborationsService = new CollaborationsService(cacheService);
+  const notesService = new NotesService(collaborationsService, cacheService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
   const storageService = new StorageService();
